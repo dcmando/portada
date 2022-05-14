da0e248dff3066a2f9df96c08de1b720ff87a8b1
arman <arman@HONOR>
2022-05-13 21:30:19 -0500

    Master

---
+++ src/main/java/com/example/portadapractica3/PortadaPractica3Application.java
@@ -0,0 +1,13 @@
+package com.example.portadapractica3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortadaPractica3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortadaPractica3Application.class, args);
+	}
+
+}
